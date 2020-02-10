b70898e1e351b3cb711c928466766a10d0165479
Christopher Pedersen <chris@topherpedersen.com>
2020-02-10 15:40:50 -0600

    changed fullstack.go to webserver.go

--- webserver.go
+++ webserver.go
@@ -1,39 +1,11 @@
-// REFERENCE (Learn Golang in 30 Minutes): https://topherpedersen.blog/2019/12/23/learn-golang-in-15-minutes/
-// REFERENCE (html/template package): https://golang.org/pkg/html/template/
-// REFERENCE (Writing Web Applications in Go): https://golang.org/doc/articles/wiki/
-
+// REFERENCE (Simple Static Web Server in Go): https://stackoverflow.com/questions/26559557/how-do-you-serve-a-static-html-file-using-a-go-web-server
 package main
 
-/*
-import(
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"html/template"
-)
-*/
-
-/*
-type WebPage struct {
-	H
-}
-
-func main() {
-	fmt.Printf("hello, golang\n")
-}
-*/
-
 import (
-    "fmt"
-    "log"
-    "net/http"
+        "net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
-
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+        http.Handle("/", http.FileServer(http.Dir("./")))
+        http.ListenAndServe(":3000", nil)
 }
